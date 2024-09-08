5b33d9ba5fbc593567a7ef99cb91400b89438bb6
AseelImad8 <aseelimad462@gmail.com>
2024-09-08 16:44:01 +0300

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [player, setPlayer] = useState({
+    firstName: 'Ranjani',
+    lastName: 'Shettar',
+    score: 10,
+  });
+
+  function handlePlusClick() {
+    setPlayer({ ...player, score: player.score + 1, })
+  }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPlayer({ ...player, [name]: value })
+  }
+
+  return (
+    <>
+      <label>
+        Score: <b>{player.score}</b>
+        {' '}
+        <button onClick={handlePlusClick}>
+          +1
+        </button>
+      </label>
+      <label>
+        First name:
+        <input
+          value={player.firstName}
+          onChange={handleChange}
+          name='firstName'
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={player.lastName}
+          onChange={handleChange}
+          name='lastName'
+        />
+      </label>
+    </>
+  );
+}
